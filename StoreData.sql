7962c8eedf2f4fb85a37b107c731a6635f6d739b
TheGuy <aaronbaw@gmail.com>
2017-12-07 22:09:36 +0000

    Add Store Dummy Data.
    Contains random staff and inventory data. No order information included.

---
+++ StoreData.sql
@@ -0,0 +1,1039 @@
+
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Chais',
+    36000,
+    250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Chang',
+    38000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Aniseed Syrup',
+    20000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Chef Antons Cajun Seasoning',
+    44000,
+    1200
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Chef Antons Gumbo Mix',
+    42000,
+    900
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Grandmas Boysenberry Spread',
+    50000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Northwoods Cranberry Sauce',
+    60000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Mishi Kobe Niku',
+    80000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Ikura',
+    194000,
+    450
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Queso Cabrales',
+    62000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Queso Manchego La Pastora',
+    42000,
+    25
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Konbu',
+    76000,
+    250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Tofu',
+    12000,
+    50
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Genen Shouyu',
+    46000,
+    1000
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Pavlova',
+    30000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Alice Mutton',
+    34000,
+    800
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Carnarvon Tigers',
+    78000,
+    500
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Teatime Chocolate Biscuits',
+    124000,
+    400
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Sir Rodneys Marmalade',
+    18000,
+    250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Sir Rodneys Scones',
+    162000,
+    750
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Gustafs Knäckebröd',
+    20000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Tunnbröd',
+    42000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Guaraná Fantástica',
+    18000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'NuNuCa Nuß-Nougat-Creme',
+    8000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Gumbär Gummibärchen',
+    28000,
+    500
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Schoggi Schokolade',
+    62000,
+    2500
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Rössle Sauerkraut',
+    86000,
+    2500
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Thüringer Rostbratwurst',
+    90000,
+    625
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Nord-Ost Matjeshering',
+    246000,
+    1250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Gorgonzola Telino',
+    50000,
+    250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Mascarpone Fabioli',
+    24000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Geitost',
+    64000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Sasquatch Ale',
+    4000,
+    12500
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Steeleye Stout',
+    28000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Inlagd Sill',
+    36000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Gravad lax',
+    38000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Côte de Blaye',
+    52000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Chartreuse verte',
+    526000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Boston Crab Meat',
+    36000,
+    18750
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Singaporean Hokkien Fried Mee',
+    36000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Ipoh Coffee',
+    18000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Gula Malacca',
+    28000,
+    800
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Røgede sild',
+    92000,
+    400
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Spegesild',
+    38000,
+    500
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Zaanse koeken',
+    18000,
+    25
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Chocolade',
+    24000,
+    100
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Maxilaku',
+    18000,
+    250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Valkoinen suklaa',
+    24000,
+    250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Manjimup Dried Apples',
+    40000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Filo Mix',
+    32000,
+    300
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Perth Pasties',
+    106000,
+    1250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Tourtière',
+    14000,
+    400
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Pâté chinois',
+    64000,
+    1200
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Gnocchi di nonna Alice',
+    14000,
+    400
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Ravioli Angelo',
+    48000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Escargots de Bourgogne',
+    76000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Raclette Courdavault',
+    38000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Camembert Pierrot',
+    26000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Sirop dérable',
+    110000,
+    125
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Tarte au sucre',
+    68000,
+    375
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Vegie-spread',
+    56000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Wimmers gute Semmelknödel',
+    98000,
+    1200
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Louisiana Hot Spiced Okra',
+    86000,
+    375
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Laughing Lumberjack Lager',
+    66000,
+    500
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Scottish Longbreads',
+    42000,
+    800
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Gudbrandsdalsost',
+    34000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Outback Lager',
+    28000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Fløtemysost',
+    24000,
+    250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Mozzarella di Giovanni',
+    72000,
+    250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Röd Kaviar',
+    30000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Longlife Tofu',
+    42000,
+    250
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Rhönbräu Klosterbier',
+    68000,
+    600
+  );
+INSERT INTO INVENTORY VALUES(
+    NULL,
+    'Lakkalikööri',
+    30000,
+    600
+  );
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Alene',
+    'Mariana'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Antonetta',
+    'Alessandra'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Edy',
+    'Jolyn'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Leesa',
+    'Loren'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Xenia',
+    'Dorthea'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Josie',
+    'Nonna'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Angelia',
+    'Britteny'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Cele',
+    'Ray'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Emmalynne',
+    'Claresta'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Ynes',
+    'Noel'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Celina',
+    'Florry'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Marijo',
+    'Sherie'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Alli',
+    'Ricky'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Donica',
+    'Olympia'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Gunilla',
+    'Heloise'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Cherey',
+    'Elene'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Valentia',
+    'Bernetta'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Daffy',
+    'Mureil'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Fernandina',
+    'Friederike'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Madeline',
+    'Aliza'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Jerry',
+    'Georgianna'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Chrysa',
+    'Dinny'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Sherilyn',
+    'Raeann'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Cairistiona',
+    'Randene'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Lindi',
+    'Nicoline'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Sherrie',
+    'Micheline'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Teresita',
+    'Mariana'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Mel',
+    'Joyann'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Tamera',
+    'Barbara'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Loralie',
+    'Krissy'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Jinny',
+    'Roanne'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Vina',
+    'Seana'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Arleta',
+    'Papagena'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Lorrayne',
+    'Lenette'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Rheba',
+    'Rubetta'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Lacy',
+    'Morena'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Kiersten',
+    'Deane'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Carolynn',
+    'Netti'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Tybie',
+    'Bennie'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Abbye',
+    'Marita'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Torie',
+    'Joellen'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Hedy',
+    'Kiah'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Barbra',
+    'Rycca'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Em',
+    'Mela'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Belia',
+    'Dorena'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Wenonah',
+    'Carita'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Eugine',
+    'Leelah'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Ferdinande',
+    'Juliane'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Lorenza',
+    'Ulrica'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Anthia',
+    'Lexine'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Wendie',
+    'Claudie'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Iseabal',
+    'Lyndell'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Devan',
+    'Fayette'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Dierdre',
+    'Tedi'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Jacquie',
+    'Livvyy'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Ira',
+    'Hedi'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Rosella',
+    'Felita'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Colly',
+    'Fayina'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Connie',
+    'Blair'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Joannes',
+    'Allison'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Rosina',
+    'Ileane'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Devonne',
+    'Prissie'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Nettle',
+    'Nannette'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Tonya',
+    'Karleen'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Carlye',
+    'Britt'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Tamarah',
+    'Konstanze'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Kellsie',
+    'Nadiya'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Jodie',
+    'Mufinella'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Ulrike',
+    'Rhona'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Trude',
+    'Xenia'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Catrina',
+    'Valera'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Jemimah',
+    'Janine'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Kori',
+    'Kary'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Lucila',
+    'Michaella'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Heath',
+    'Catlin'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Jacky',
+    'Gabriell'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Jean',
+    'Cynde'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Arlette',
+    'Mae'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Brenda',
+    'Shandie'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Elvera',
+    'Gabriella'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Britteny',
+    'Pennie'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Gerti',
+    'Marrissa'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Adrian',
+    'Alexandrina'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Amil',
+    'Celestine'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Dolorita',
+    'Tersina'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Zilvia',
+    'Pierrette'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Noami',
+    'Cherise'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Selestina',
+    'Kirsteni'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Vivianna',
+    'Dyanne'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Barbe',
+    'Dalia'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Aimee',
+    'Merci'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Jo-Anne',
+    'Charil'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Angelle',
+    'Gretna'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Ealasaid',
+    'Ranique'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Jaimie',
+    'Raynell'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Margalit',
+    'Amber'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Emeline',
+    'Tara'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Cymbre',
+    'Herminia'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Malva',
+    'Pauline'
+  );
+  
+  INSERT INTO STAFF VALUES(
+    NULL,
+    'Robbin',
+    'Therine'
+  );
+  
